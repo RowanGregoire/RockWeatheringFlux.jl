0eb53fcd9de92be692f4c8b0811fd381058f436c
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2025-11-25 14:41:51 -0500

    demo code to make new results

---
+++ demos/make_new_results.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e  # Fail if any command fails 
+
+# Lithology classifications and screening -- comment out to save some time if you trust 
+# the existing sorting + screening that we've done. Re-do this if you want to double-check 
+# anything or if you've changed any of the sorting algos or rock type wordbanks 
+julia --project="Project.toml" dev/fixmacrostrat.jl     # Macrostrat lithology
+julia --project="Project.toml" src/ScreenCombined.jl    # Combined geochemistry 
+
+# Using existing lith classes and geochemical screenings, re-run for results 
+julia --project="Project.toml" src/CalculateFlux.jl
+julia --project="Project.toml" src/UpperCrustComposition.jl  
