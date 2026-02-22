feffd60787e0ed78ea70954a7150437ccfd48a50
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2026-02-22 13:27:21 -0500

    Make this a start to finish code run

--- demos/make_new_results.sh
+++ demos/make_new_results.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e  # Fail if any command fails 
 
-# Lithology classifications and screening -- comment out to save some time if you trust 
-# the existing sorting + screening that we've done. Re-do this if you want to double-check 
-# anything or if you've changed any of the sorting algos or rock type wordbanks 
-julia --project="Project.toml" src/ReclassMacrostrat.jl  # Macrostrat lithology
-julia --project="Project.toml" src/ScreenCombined.jl     # Combined geochemistry 
+# Lithology classifications and screening -- comment out to save some time if you've already 
+# gotten the initial datasets set up. 
+julia --project="Project.toml" src/ParseMacrostrat.jl
+julia --project="Project.toml" src/SRTMSlope.jl
+julia --project="Project.toml" src/ModelSlope.jl
+julia --project="Project.toml" src/ScreenCombined.jl 
+
+# Uncomment to re-do the Macrostrat rock classification: 
+# julia --project="Project.toml" src/ReclassMacrostrat.jl
 
 # Using existing lith classes and geochemical screenings, re-run for results 
 julia --project="Project.toml" src/SampleMatch.jl
