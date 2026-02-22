d072b902e0b3104026f847c44c302a98a3e49972
Rowan Gregoire <rowan.k.gregoire.23@dartmouth.edu>
2026-02-22 12:55:17 -0500

    Update script names

--- demos/make_new_results.sh
+++ demos/make_new_results.sh
@@ -4,8 +4,8 @@ set -e  # Fail if any command fails
 # Lithology classifications and screening -- comment out to save some time if you trust 
 # the existing sorting + screening that we've done. Re-do this if you want to double-check 
 # anything or if you've changed any of the sorting algos or rock type wordbanks 
-julia --project="Project.toml" dev/fixmacrostrat.jl     # Macrostrat lithology
-julia --project="Project.toml" src/ScreenCombined.jl    # Combined geochemistry 
+julia --project="Project.toml" src/ReclassMacrostrat.jl  # Macrostrat lithology
+julia --project="Project.toml" src/ScreenCombined.jl     # Combined geochemistry 
 
 # Using existing lith classes and geochemical screenings, re-run for results 
 julia --project="Project.toml" src/SampleMatch.jl
